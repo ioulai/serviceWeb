735f0fba443dc8559841453fced2ad0314ecac1d
ioulai <oulaiisaac@gmail.com>
2017-03-20 17:23:40 +0100

    Update routes.php

--- webService/src/routes.php
+++ webService/src/routes.php
@@ -16,16 +16,9 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 		$response->getBody()->write("hello, $name,$prenom");
 		return $response;
 	});
-		//connexion à l'application
-		$app->get('/getConnect/{login}/{mp}', function ($request, $response, $args) {
-			$sth = $this->db->prepare("SELECT * FROM personne_login where login = :login and mp = :mp");
-			$sth->bindParam("login", $args['login']);
-			$sth->bindParam("mp", $args['mp']);
-			$sth->execute();
-			$todos = $sth->fetchObject();
-			return $this->response->withJson($todos);
-		});
-		
+/**************************************************************************************************************************************************/	
+/************************************************************** REQUETE GET ***********************************************************************/
+/**************************************************************************************************************************************************/	
 	
 	    //recuperer nom prenom en fonction du login et mp
 		$app->get('/getPersInfo/{login}/{mp}', function ($request, $response, $args) {
@@ -37,9 +30,76 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 			return $this->response->withJson($todos);
 			});
 		
+		//connexion Ã  l'application	
+			$app->get('/getConnect/{login}/{mp}', function ($request, $response, $args) {
+			$sth = $this->db->prepare("SELECT * FROM personne_login where login = :login and mp = :mp");			
+			$sth->bindParam("login", $args['login']);
+			$sth->bindParam("mp", $args['mp']);
+			$sth->execute();
+			$id = $this->db->prepare("SELECT id FROM personne_login where login = :login and mp = :mp");
+			$id->bindParam("login", $args['login']);
+			$id->bindParam("mp", $args['mp']);
+			$id->execute();
+			$type='';
+			
+			$todos = $sth->fetchObject();			
+			return $this->response->withJson($todos);
+		});	
+/**************************************************************FIN REQUETE GET**********************************************************************/			
+
+			
 	
 			
+/**************************************************************************************************************************************************/			
+/************************************************************** REQUETE PUT ***********************************************************************/		
+/**************************************************************************************************************************************************/
+	/*	//Modification patient
+		$app->put('/setPatient/{id}', function ($request, $response, $args){
+			$sth = $this->db->prepare("UPDATE patient SET informations_medicales = :informations_medicales, personne_de_confiance = :personne_de_confiance, infirmiere_souhait = :infirmiere_souhait  where p.id = :id");
+			$sth->bindParam("id", $args['id']);
+			$sth->bindParam("informations_medicales", $args['informations_medicales']);
+			$sth->bindParam("personne_de_confiance", $args['personne_de_confiance']);
+			$sth->bindParam("infirmiere_souhait", $args['infirmiere_souhait']);
+			$sth->execute();
+			$todos = $sth->fetchObject();
+			return $this->response->withJson($todos);
+			
+		});
 		
+		//modification mot de passe patient
+		$app->put('/setMpPatient/{login}', function ($request, $response, $args){
+			$sth = $this->db->prepare("UPDATE personne_login set mp = :mp where login = :login ");
+			$sth->bindParam("login", $args['login']);
+			$sth->bindParam("mp", $args['mp']);
+			$sth->execute();
+			$todos = $sth->fetchObject();
+			return $this->response->withJson($todos);
+		});*/
+
+/************************************************************** FIN REQUETE PUT ********************************************************************/			
+
+
+/**************************************************************************************************************************************************/
+/************************************************************** REQUETE POST **********************************************************************/
+/**************************************************************************************************************************************************/		
+	
+		
+		
+		
+/*********************************************************** FIN REQUETE POST **********************************************************************/
+		
+	
+		
+		
+		
+/**************************************************************************************************************************************************/		
+/************************************************************** REQUETE DELETE ********************************************************************/
+/**************************************************************************************************************************************************/			
+
+		
+		
+			
+/************************************************************* FIN REQUETE DELETE ******************************************************************/
 	
 		
 	
