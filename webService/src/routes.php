471e36eaa3baf05ef9bdc934e9d2cc5f84e3cff1
ioulai <ioulai@ST206PC.BTSInfo.edu>
2017-04-24 13:31:12 +0200

    ajout methodes

--- webService/src/routes.php
+++ webService/src/routes.php
@@ -279,6 +279,7 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 				->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
 				->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 				->write(json_encode($data));
+				
 			});
 			
 		
